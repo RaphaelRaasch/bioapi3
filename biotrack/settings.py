53ed132a38edbe40e73bede7fe8a142444a98943
RaphaelRaasch <raaschfialho@gmail.com>
2020-06-12 15:09:21 -0300

    gestao1

--- biotrack/settings.py
+++ biotrack/settings.py
@@ -80,13 +80,13 @@ WSGI_APPLICATION = 'biotrack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}'''
-DATABASES = {
+}
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'multidev',
@@ -95,7 +95,7 @@ DATABASES = {
         'HOST': 'mysql.multidev.com.br',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
-}
+}'''
 
 
 # Password validation
